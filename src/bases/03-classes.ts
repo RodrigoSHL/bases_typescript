0f29baa69e0ae49c51603365f561699897b4428a
RodrigoSHL <shl.catalan@gmail.com>
2022-08-16 19:21:42 -0400

    add: getters, method, this
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,6 +1,22 @@
 export class Pokemon {
+  get imageUrl(): string {
+    return `https://www.pokemon.com/${this.id}.jpg`;
+  }
   constructor(public readonly id: number, public name: string) {}
+
+  scream() {
+    console.log(`${this.name.toUpperCase()}!!!`);
+  }
+
+  speak() {
+    console.log(`${this.name}`, `${this.name}`);
+  }
 }
 
 export const charmander = new Pokemon(4, "Charmander");
 
+console.log("charmander", charmander.imageUrl);
+
+
+charmander.scream()
+charmander.speak()

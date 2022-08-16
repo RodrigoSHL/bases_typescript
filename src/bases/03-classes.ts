542c191e75cb905e6dfa048d9168ca6a3b2ab679
RodrigoSHL <shl.catalan@gmail.com>
2022-08-16 19:42:35 -0400

    add: interface and install axios
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/bases/03-classes.ts
+++ src/bases/03-classes.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { IPokeapi, Move } from "../interface/pokeapi.interface";
+
 export class Pokemon {
   get imageUrl(): string {
     return `https://www.pokemon.com/${this.id}.jpg`;
@@ -11,12 +14,19 @@ export class Pokemon {
   speak() {
     console.log(`${this.name}`, `${this.name}`);
   }
+
+  async getMoves(): Promise<Move[]> {
+    const {data} = await axios.get<IPokeapi>("https://pokeapi.co/api/v2/pokemon/4");
+    console.log("res", data.moves);
+    return data.moves;
+  }
 }
 
 export const charmander = new Pokemon(4, "Charmander");
 
 console.log("charmander", charmander.imageUrl);
 
+// charmander.scream();
+// charmander.speak();
 
-charmander.scream()
-charmander.speak()
+console.log(charmander.getMoves());

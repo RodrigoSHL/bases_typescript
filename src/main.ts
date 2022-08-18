66e5825c68ceadb7c932ab5e6adc5440f1414cfe
RodrigoSHL <shl.catalan@gmail.com>
2022-08-17 21:04:01 -0400

    add: decorators added
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
-import { age, name } from "./bases/01-types";
-import { bulbasaur, pokemonIds } from "./bases/02-objects";
-import { charmander } from "./bases/04-injections";
-
+// import { age, name } from "./bases/01-types";
+// import { bulbasaur, pokemonIds } from "./bases/02-objects";
+// import { charmander } from "./bases/04-injections";
+import { charmander } from "./bases/05-decorators";
 import "./style.css";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 app.innerHTML = `
   <div>
-    <h2>01 ${name}, ${age}</h2>
-    <h2>Hello ${bulbasaur.name}</h2>
     <h2>Hello ${charmander.name}</h2>
 
     <a href="https://vitejs.dev" target="_blank">Documentation</a>
